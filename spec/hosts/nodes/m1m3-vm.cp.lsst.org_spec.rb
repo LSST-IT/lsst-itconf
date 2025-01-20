df8d8ff75b4616bf84f320d2641e8cfc19e53db4
Ivan Gonzalez <igonzalez@lsst.org>
2025-01-20 17:49:25 -0300

    (node/m1m3-vm.cp) update m1m3-vm configuration

--- spec/hosts/nodes/m1m3-vm.cp.lsst.org_spec.rb
+++ spec/hosts/nodes/m1m3-vm.cp.lsst.org_spec.rb
@@ -27,15 +27,15 @@ describe 'm1m3-vm.cp.lsst.org', :sitepp do
       it { is_expected.to compile.with_all_deps }
 
       include_context 'with nm interface'
-      it { is_expected.to have_nm__connection_resource_count(2) }
+      it { is_expected.to have_nm__connection_resource_count(1) }
 
       context 'with ens192' do
         let(:interface) { 'ens192' }
 
         it_behaves_like 'nm enabled interface'
         it_behaves_like 'nm ethernet interface'
-        it { expect(nm_keyfile['ipv4']['address1']).to eq('139.229.170.85/24,139.229.170.254') }
-        it { expect(nm_keyfile['ipv4']['dns']).to eq('139.229.135.53;139.229.135.54;139.229.135.55;') }
+        it { expect(nm_keyfile['ipv4']['address1']).to eq('139.229.170.55/24,139.229.170.254') }
+        it { expect(nm_keyfile['ipv4']['dns']).to eq('139.229.160.53;139.229.160.54;139.229.160.55;') }
         it { expect(nm_keyfile['ipv4']['dns-search']).to eq('cp.lsst.org;') }
         it { expect(nm_keyfile['ipv4']['method']).to eq('manual') }
       end
